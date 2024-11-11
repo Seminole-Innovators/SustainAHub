78fb182d0b0bd4a7ba1f07d696e65c20f93bf437
Nelson <nam24l@fsu.edu>
2024-11-11 11:46:14 -0800

    Added small map and calendar. We need to figure out the events and get icons for the locations.

--- public/script.js
+++ public/script.js
@@ -16,3 +16,101 @@ function scrollToSection(sectionId) {
 
 // Hello everyone! 
 
+// Function to get data from api 
+
+const fetchData = async (latitude, longitude) => {
+    const response = await fetch('/airquality', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            lat: latitude,
+            lon: longitude,
+        }),
+    });
+  
+    const data = await response.json();
+    console.log(data);
+  };
+  
+  // Map
+  async function geoFindMe() {
+  
+      let LeafIcon = L.Icon.extend({
+          options: {
+              shadowUrl: 'leaf-shadow.png',
+              iconSize:     [38, 95],
+              shadowSize:   [50, 64],
+              iconAnchor:   [22, 94],
+              shadowAnchor: [4, 62],
+              popupAnchor:  [-3, -76]
+          }
+      });
+  
+      let greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'});
+  
+  
+      function success(position) {
+        // get coordinates 
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+  
+        // gets data 
+        // Call the function
+        fetchData(latitude, longitude);
+  
+          var map = L.map('small-map').setView([latitude, longitude], 13);
+  
+          L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+              maxZoom: 19,
+              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          }).addTo(map);
+  
+          L.marker([latitude, longitude], {icon: greenIcon}).addTo(map).bindPopup("Your Current Location.");
+          
+          // Place parks on map
+          fetch('./data/Parks.geojson')
+          .then(response => response.json())
+          .then(jsonData => {
+              for (let i = 0; i < jsonData.features.length; i++) {
+                  L.marker([jsonData.features[i].properties.YCOORD, jsonData.features[i].properties.XCOORD]).addTo(map).bindPopup(`${jsonData.features[i].properties.PARKNAME}`);
+              }
+          }); 
+          
+          // Place Sustainability Locations on Map
+          fetch('./data/Sustainability_Locations.geojson')
+          .then(response => response.json())
+          .then(jsonData => {
+              for (let i = 0; i < jsonData.features.length; i++) {
+                  L.marker([jsonData.features[i].geometry.coordinates[1], jsonData.features[i].geometry.coordinates[0]]).addTo(map).bindPopup(`${jsonData.features[i].properties.NAME}`);
+              }
+          });
+      }
+    
+      function error() {
+        alert("Unable to retrieve your location.");
+      }
+  
+      const options = {
+          enableHighAccuracy: true,
+      };
+    
+      if (!navigator.geolocation) {
+        alert("Geolocation is not supported by your browser.)");
+      } else {
+        navigator.geolocation.getCurrentPosition(success, error, options);
+      }
+      
+  }
+    
+  geoFindMe();
+
+  // Calendar 
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('small-calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+    });
+    calendar.render();
+  });
